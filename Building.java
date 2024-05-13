df7a155d69b3dd4a6b253a80c89985ee220caf26
HoweyLuong <165950202+HoweyLuong@users.noreply.github.com>
2024-05-13 15:42:44 -0700

    add 2 new files

---
+++ Building.java
@@ -0,0 +1,13 @@
+package GroupProject;
+import java.sql.*;
+public class Building {
+	public int buildingID;
+	
+	public int getBuildingID() {
+		return buildingID;
+	}
+	
+	public void setBuildingID(int buildingID) {
+		 this.buildingID= buildingID;
+	}
+}
